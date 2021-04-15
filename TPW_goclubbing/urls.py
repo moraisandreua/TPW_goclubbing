88a98c25aaacc15f611cd0b5eb62da41594fd88c
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2021-04-15 10:58:03 +0100

    major update
    - maker opens sidebar
    
    to-do:
    - main page design
    - filter options
    - sidebar remain items

--- TPW_goclubbing/urls.py
+++ TPW_goclubbing/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import index, search
+from app.views import index, search, searchName
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('search/', search),
+    path('search/<str:id>', searchName),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import folium
-from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
@@ -10,12 +9,26 @@ def search(request):
     f = folium.Figure(width=1000, height=1000)
     m = folium.Map([41.120736, -8.611354], zoom_start=25).add_to(f)
     folium.TileLayer('cartodbpositron').add_to(m)
-    pp = folium.Html('<a href="' + 'give your url here' + '"target="_blank">' + 'popup text' + '</a>', script=True)
+
+    pp = folium.Html("<a onclick='javascript:parent.myfunction();' style='cursor:pointer;'>Bar do Fred</a>", script=True)
     popup = folium.Popup(pp, max_width=2650)
     folium.Marker(location=[41.120736, -8.611354], popup=popup).add_to(m)
     # depois aqui coloca-se os vários markers da base de dados
     m = m._repr_html_()
     context = {'my_map': m}
+
+    return render(request, 'search.html', context)
+
+def searchName(request, id):
+    f = folium.Figure(width=1000, height=1000)
+    m = folium.Map([41.120736, -8.611354], zoom_start=25).add_to(f)
+    folium.TileLayer('cartodbpositron').add_to(m)
+    pp = folium.Html('<a id="openmodals" onclick="openModal()">' + 'Bar do Fred' + '</a>', script=True)
+    popup = folium.Popup(pp, max_width=2650)
+    folium.Marker(location=[41.120736, -8.611354], popup=popup).add_to(m)
+    # depois aqui coloca-se os vários markers da base de dados
+    m = m._repr_html_()
+    context = {'my_map': m, 'name':id}
 
     return render(request, 'search.html', context)
 
