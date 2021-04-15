1d568564f8332bc876300f7a13456f5dcc8f706a
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2021-04-15 09:34:12 +0100

    major update
    map

--- TPW_goclubbing/urls.py
+++ TPW_goclubbing/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import index, search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('search/', search),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import folium
+from django.views.generic import TemplateView
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html", {})
+
+def search(request):
+    f = folium.Figure(width=1000, height=1000)
+    m = folium.Map([41.120736, -8.611354], zoom_start=25).add_to(f)
+    folium.TileLayer('cartodbpositron').add_to(m)
+    pp = folium.Html('<a href="' + 'give your url here' + '"target="_blank">' + 'popup text' + '</a>', script=True)
+    popup = folium.Popup(pp, max_width=2650)
+    folium.Marker(location=[41.120736, -8.611354], popup=popup).add_to(m)
+    # depois aqui coloca-se os vários markers da base de dados
+    m = m._repr_html_()
+    context = {'my_map': m}
+
+    return render(request, 'search.html', context)
+
+
