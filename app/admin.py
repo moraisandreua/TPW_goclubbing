eb49c42bd528506c76a94bf3440e9de407efc092
Miguel Marques <miguelmarques@MacBook-Air-de-Miguel-2.local>
2021-04-17 23:06:09 +0100

    model registrations

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app.models import Business, BusinessPhoto, EventPhoto, Event, Comment
 
 # Register your models here.
+admin.site.register(Business)
+admin.site.register(BusinessPhoto)
+admin.site.register(EventPhoto)
+admin.site.register(Event)
+admin.site.register(Comment)
